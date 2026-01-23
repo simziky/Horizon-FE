104ede5aa5aa74bb0c8dba0511f47bfedd73d729
simziky <simeonehioze@gmail.com>
2026-01-23 16:26:40 +0100

    update: build fix

--- app/(dashboard)/totan/_components/Totan.tsx
+++ app/(dashboard)/totan/_components/Totan.tsx
@@ -1,4 +1,5 @@
 "use client";
+/* eslint-disable @typescript-eslint/no-unused-vars*/
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { useEffect, useRef, useState } from "react";
 import { BsStars } from "react-icons/bs";
